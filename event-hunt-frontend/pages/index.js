85f2f7cbf4407c6699e1dae53c68480d0f7a58f8
RdotSilva <RyandotSilva@gmail.com>
2021-07-23 13:54:27 -0400

    Add logic to render events

--- event-hunt-frontend/pages/index.js
+++ event-hunt-frontend/pages/index.js
@@ -1,10 +1,16 @@
+import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
-import { API_URL } from "*/config/index";
+import { API_URL } from "@/config/index";
 
 export default function Home({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
 }
