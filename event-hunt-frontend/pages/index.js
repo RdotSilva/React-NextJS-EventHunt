6969282dee60f1cd5441b26519cd1af6ec08d9ed
RdotSilva <RyandotSilva@gmail.com>
2021-07-23 13:47:52 -0400

    Add getStaticProps method

--- event-hunt-frontend/pages/index.js
+++ event-hunt-frontend/pages/index.js
@@ -1,9 +1,25 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "*/config/index";
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+/**
+ * Hit the Events API at build time and return the events data as props
+ * Only runs once (for DEV env)
+ */
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
