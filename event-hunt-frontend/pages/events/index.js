43ea875a97611f261243f6da3595aeb7b9f74d7f
RdotSilva <RyandotSilva@gmail.com>
2021-07-24 13:37:56 -0400

    Add EventsPage component

--- event-hunt-frontend/pages/events/index.js
+++ event-hunt-frontend/pages/events/index.js
@@ -1,9 +1,31 @@
+import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+/**
+ * Hit the Events API at build time and return the events data as props
+ * Only runs once (for DEV env)
+ */
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
