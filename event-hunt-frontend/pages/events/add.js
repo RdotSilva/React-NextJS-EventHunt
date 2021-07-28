121ffe09122b3857a3f2d995a08df77117e78bab
RdotSilva <RyandotSilva@gmail.com>
2021-07-28 15:10:00 -0400

    Add state for event values

--- event-hunt-frontend/pages/events/add.js
+++ event-hunt-frontend/pages/events/add.js
@@ -1,8 +1,25 @@
 import Layout from "@/components/Layout";
+import styles from "@/styles/Form.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function AddEventPage() {
+  const [values, setValues] = useState({
+    name: "",
+    performers: "",
+    venue: "",
+    address: "",
+    date: "",
+    time: "",
+    description: "",
+  });
+
+  const router = useRouter();
+
   return (
     <Layout title="Add New Event">
+      <Link href="events">Go Back</Link>
       <h1>Add Event</h1>
     </Layout>
   );
