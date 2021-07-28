4bc7cb1a7ab3e326b9b5595c8e7c081f57e1d95e
RdotSilva <RyandotSilva@gmail.com>
2021-07-28 15:04:08 -0400

    Add handleSubmit method

--- event-hunt-frontend/components/Search.js
+++ event-hunt-frontend/components/Search.js
@@ -7,6 +7,16 @@ export default function Search() {
 
   const router = useRouter();
 
+  /**
+   * Route the user to th search page and reset state
+   * @param {object} e The input event
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/events/search?term=${term}`);
+    setTerm("");
+  };
+
   return (
     <div className={styles.search}>
       <form onSubmit={handleSubmit}>
