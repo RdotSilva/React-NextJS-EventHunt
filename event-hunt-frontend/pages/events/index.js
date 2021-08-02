9c7fdbff53c250d1595a15c3aa40cf634038e665
RdotSilva <RyandotSilva@gmail.com>
2021-08-02 14:49:42 -0400

    Add logic to fetch paginated results

--- event-hunt-frontend/pages/events/index.js
+++ event-hunt-frontend/pages/events/index.js
@@ -2,7 +2,10 @@ import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 
-export default function EventsPage({ events }) {
+const RESULTS_PER_PAGE = 2;
+
+export default function EventsPage({ events, page, total }) {
+  const lastPage = Math.ceil(total / RESULTS_PER_PAGE);
   return (
     <Layout>
       <h1>Events</h1>
@@ -16,17 +19,27 @@ export default function EventsPage({ events }) {
 }
 
 /**
- * Hit the Events API at build time and return the events data as props
- * Only runs once (for DEV env)
+ * Hit the Events API and return the events data as props
  */
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-  const events = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  // Calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * RESULTS_PER_PAGE;
+
+  // Fetch total/count of events
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = await totalRes.json();
+
+  // Fetch events
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${RESULTS_PER_PAGE}&_start=${start}`
+  );
+  const events = await eventRes.json();
 
   return {
     props: {
       events,
+      page: +page,
+      total,
     },
-    revalidate: 1,
   };
 }
