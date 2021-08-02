0508e2ee1e040e572596adeded61162517f77010
RdotSilva <RyandotSilva@gmail.com>
2021-08-02 15:11:48 -0400

    Refactor forms

--- event-hunt-frontend/pages/events/add.js
+++ event-hunt-frontend/pages/events/add.js
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import EventForm from "@/components/EventForm";
 import styles from "@/styles/Form.module.css";
 import Link from "next/link";
 import { ToastContainer, toast } from "react-toastify";
@@ -70,79 +71,7 @@ export default function AddEventPage() {
       <h1>Add Event</h1>
       <ToastContainer />
       <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.grid}></div>
-        <div>
-          <label htmlFor="name">Event Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={values.name}
-            onChange={handleInputChange}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="performers">Performers</label>
-          <input
-            type="text"
-            name="performers"
-            id="performers"
-            value={values.performers}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="venue">Venue</label>
-          <input
-            type="text"
-            name="venue"
-            id="venue"
-            value={values.venue}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            type="text"
-            name="address"
-            id="address"
-            value={values.address}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            name="date"
-            id="date"
-            value={values.date}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="time">Time</label>
-          <input
-            type="text"
-            name="time"
-            id="time"
-            value={values.time}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="description">Event Description</label>
-          <textarea
-            type="text"
-            name="description"
-            id="description"
-            value={values.description}
-            onChange={handleInputChange}
-          ></textarea>
-        </div>
-
+        <EventForm values={values} handleInputChange={handleInputChange} />
         <input type="submit" value="Add Event" className="btn" />
       </form>
     </Layout>
--- event-hunt-frontend/pages/events/edit/[id].js
+++ event-hunt-frontend/pages/events/edit/[id].js
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import Modal from "@/components/Modal";
+import EventForm from "@/components/EventForm";
 import ImageUpload from "@/components/ImageUpload";
 import { FaImage } from "react-icons/fa";
 import styles from "@/styles/Form.module.css";
@@ -10,7 +11,6 @@ import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { API_URL } from "@/config/index";
-import moment from "moment";
 
 export default function EditEventPage({ evt }) {
   const [values, setValues] = useState({
@@ -91,79 +91,7 @@ export default function EditEventPage({ evt }) {
       <h1>Edit Event</h1>
       <ToastContainer />
       <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.grid}></div>
-        <div>
-          <label htmlFor="name">Event Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={values.name}
-            onChange={handleInputChange}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="performers">Performers</label>
-          <input
-            type="text"
-            name="performers"
-            id="performers"
-            value={values.performers}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="venue">Venue</label>
-          <input
-            type="text"
-            name="venue"
-            id="venue"
-            value={values.venue}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            type="text"
-            name="address"
-            id="address"
-            value={values.address}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            name="date"
-            id="date"
-            value={moment(values.date).format("yyyy-MM-DD")}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="time">Time</label>
-          <input
-            type="text"
-            name="time"
-            id="time"
-            value={values.time}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="description">Event Description</label>
-          <textarea
-            type="text"
-            name="description"
-            id="description"
-            value={values.description}
-            onChange={handleInputChange}
-          ></textarea>
-        </div>
-
+        <EventForm values={values} handleInputChange={handleInputChange} />
         <input type="submit" value="Update Event" className="btn" />
       </form>
 
