57619c2f1e263cfcf5849cfacf8acd67a38cf349
RdotSilva <RyandotSilva@gmail.com>
2021-08-04 14:59:59 -0400

    Add email form input

--- event-hunt-frontend/pages/account/register.js
+++ event-hunt-frontend/pages/account/register.js
@@ -1,3 +1,33 @@
+import { FaUser } from "react-icons/fa";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState, useEffect, useContext } from "react";
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import styles from "@/styles/AuthForm.module.css";
+
 export default function RegisterPage() {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+
+  return (
+    <Layout title="User Registration">
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
 }
