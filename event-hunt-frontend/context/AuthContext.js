15c9da4b5eb7f6441e958831a5fe76d86b5ff7fa
RdotSilva <RyandotSilva@gmail.com>
2021-08-05 15:13:58 -0400

    Add placeholder methods

--- event-hunt-frontend/context/AuthContext.js
+++ event-hunt-frontend/context/AuthContext.js
@@ -8,6 +8,25 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  // Register User
+  const register = async (user) => {
+    console.log(user);
+  };
+  // Login User
+  const login = async ({ email: identifier, password }) => {
+    console.log({ identifier, password });
+  };
+
+  // Logout User
+  const logout = async () => {
+    console.log("Logout");
+  };
+
+  // Check if user is logged in (so that user can persist through refreshes)
+  const checkUserLoggedIn = async (user) => {
+    console.log("Checked");
+  };
+
   return (
     <AuthContext.Provider value={{ user, error, register, login, logout }}>
       {children}
