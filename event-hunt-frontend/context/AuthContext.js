492a5d7a4b71b8187896393557a4e54f65df67e2
RdotSilva <RyandotSilva@gmail.com>
2021-08-05 15:13:48 -0400

    Add AuthProvider component

---
+++ event-hunt-frontend/context/AuthContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { API_URL } from "@/config/index";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  return (
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
