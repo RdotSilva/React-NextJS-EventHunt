ac9d4cf158d82575366831886d9ddf9a7171bb04
RdotSilva <RyandotSilva@gmail.com>
2021-08-05 15:21:51 -0400

    Add auth context logic

--- event-hunt-frontend/components/Header.js
+++ event-hunt-frontend/components/Header.js
@@ -1,9 +1,13 @@
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { useContext } from "react";
 import Link from "next/link";
-import styles from "@/styles/Header.module.css";
 import Search from "./Search";
+import AuthContext from "@/context/AuthContext";
+import styles from "@/styles/Header.module.css";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -11,7 +15,9 @@ export default function Header() {
           <a>Event Hunt</a>
         </Link>
       </div>
+
       <Search />
+
       <nav>
         <ul>
           <li>
@@ -19,14 +25,38 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <Link href="/events/add">
-            <a>Add Event</a>
-          </Link>
-          <Link href="/account/login">
-            <a className="btn-secondary btn-icon">
-              <FaSignInAlt /> Login
-            </a>
-          </Link>
+          {user ? (
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
