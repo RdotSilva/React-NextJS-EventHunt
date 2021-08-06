73c8d05e166777ba7295e43a00fa9a21471cfa02
RdotSilva <RyandotSilva@gmail.com>
2021-08-06 12:53:12 -0400

    Add TODO to set cookie

--- event-hunt-frontend/pages/api/login.js
+++ event-hunt-frontend/pages/api/login.js
@@ -4,9 +4,28 @@ export default async (req, res) => {
   if (req.method === "POST") {
     const { identifier, password } = req.body;
 
-    // TODO: Update logic to log user in, this will be called from AuthContext
-    console.log(req.body);
-    res.status(200).json({});
+    // Strap middleman route to set HTTP only cookie with the token
+    const strapiRes = await fetch(`${API_URL}/auth/local`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    });
+
+    const data = await strapiRes.json();
+
+    if (strapiRes.ok) {
+      // TODO: Set cookie
+      res.status(200).json({ user: data.user });
+    } else {
+      res
+        .status(data.statusCode)
+        .json({ message: data.message[0].messages[0].message });
+    }
   } else {
     res.setHeader("Allow", ["POST"]);
     res.status(405).json({ message: `Method ${req.method} not allowed` });
