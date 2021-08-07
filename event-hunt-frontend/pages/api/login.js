7ca235e94db05efb3534288fd41af7fc2f4f3b50
RdotSilva <RyandotSilva@gmail.com>
2021-08-07 15:07:55 -0400

    Add logic to set cookie

--- event-hunt-frontend/pages/api/login.js
+++ event-hunt-frontend/pages/api/login.js
@@ -1,4 +1,6 @@
 import { API_URL } from "@/config/index";
+import { strict } from "assert";
+import cookie from "cookie";
 
 export default async (req, res) => {
   if (req.method === "POST") {
@@ -19,7 +21,19 @@ export default async (req, res) => {
     const data = await strapiRes.json();
 
     if (strapiRes.ok) {
-      // TODO: Set cookie
+      // Set cookie server side
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 24 * 7, // 1 week
+          sameSite: strict,
+          path: "/",
+        })
+      );
+
+      // Send back the user
       res.status(200).json({ user: data.user });
     } else {
       res
