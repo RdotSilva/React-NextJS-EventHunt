80b6c8b6bf57147ad803a8180bf1761578cfd94d
RdotSilva <RyandotSilva@gmail.com>
2021-08-08 14:22:26 -0400

    Update logic to route user to dashboard

--- event-hunt-frontend/context/AuthContext.js
+++ event-hunt-frontend/context/AuthContext.js
@@ -8,6 +8,8 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+
   /**
    * Check if user is logged in
    */
@@ -52,6 +54,7 @@ export const AuthProvider = ({ children }) => {
 
     if (res.ok) {
       setUser(data.user);
+      router.push("/account/dashboard");
     } else {
       setUser(null);
     }
