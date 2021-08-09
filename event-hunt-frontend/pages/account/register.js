fcc23540645ab4d34e663f3e1a5cf15841d8e027
RdotSilva <RyandotSilva@gmail.com>
2021-08-09 14:47:19 -0400

    Fix typo

--- event-hunt-frontend/pages/account/register.js
+++ event-hunt-frontend/pages/account/register.js
@@ -78,7 +78,7 @@ export default function RegisterPage() {
               onChange={(e) => setPasswordConfirm(e.target.value)}
             ></input>
           </div>
-          <input type="submit" value="Login" className="btn" />
+          <input type="submit" value="Register" className="btn" />
         </form>
         <p>
           Already have an account? <Link href="/account/login">Login</Link>
