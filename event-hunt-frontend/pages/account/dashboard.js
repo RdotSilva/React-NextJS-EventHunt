bf17d43e3c648a4b6a0a05fad1ad9aad01d2d498
RdotSilva <RyandotSilva@gmail.com>
2021-08-11 15:02:55 -0400

    Add getServerSideProps method

--- event-hunt-frontend/pages/account/dashboard.js
+++ event-hunt-frontend/pages/account/dashboard.js
@@ -1,4 +1,6 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/";
+import { parseCookies } from "@/helpers/index";
 
 export default function Dashboard() {
   return (
@@ -6,4 +8,25 @@ export default function Dashboard() {
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+/**
+ * Get the users token using the parseCookies util
+ * Fetch user events and populate props with events
+ */
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
 }
