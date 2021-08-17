d7baf10f2ce5a6b938d7291734177d168170dcfe
RdotSilva <RyandotSilva@gmail.com>
2021-08-17 14:34:25 -0400

    Add GitHub link to about page

--- event-hunt-frontend/pages/about.js
+++ event-hunt-frontend/pages/about.js
@@ -1,7 +1,8 @@
 export default function AboutPage() {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>About</h1>
+      <a href="https://github.com/RdotSilva/React-NextJS-EventHunt">GitHub</a>
+    </div>
+  );
 }
