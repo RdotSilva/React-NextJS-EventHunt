3d345b3a94dea5cfc9a999dda612c910e08ee58a
RdotSilva <RyandotSilva@gmail.com>
2021-08-17 14:29:00 -0400

    Update pagination view

--- event-hunt-frontend/components/Pagination.js
+++ event-hunt-frontend/components/Pagination.js
@@ -3,18 +3,18 @@ import { RESULTS_PER_PAGE } from "@/config/index";
 
 export default function Pagination({ page, total }) {
   const lastPage = Math.ceil(total / RESULTS_PER_PAGE);
-  
+
   return (
     <>
       {page > 1 && (
         <Link href={`/events?page=${page - 1}`}>
-          <a className="btn-secondary">Prev</a>
+          <a className="btn-secondary">Prev ({page - 1})</a>
         </Link>
       )}
 
       {page < lastPage && (
         <Link href={`/events?page=${page + 1}`}>
-          <a className="btn-secondary">Next</a>
+          <a className="btn-secondary">Next ({page + 1})</a>
         </Link>
       )}
     </>
